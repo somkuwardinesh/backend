e5525fd3cf3cc81caa7d8cdf1fb4eafc16d9286d
somkuwardinesh <somkuwardinesh@yahoo.com>
2024-01-21 13:55:20 +0530

    Update EmpController.java

--- src/main/java/com/test/simplerest/EmpController.java
+++ src/main/java/com/test/simplerest/EmpController.java
@@ -23,7 +23,7 @@ import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/emp")
-@CrossOrigin(origins = "http://34.66.113.116/")
+@CrossOrigin(origins = "*")
 @Log4j2
 public class EmpController {
 
