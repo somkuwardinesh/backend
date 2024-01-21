b0d0d35ce7885f7fb1362236f72d62f02734c85d
Dinesh <somkuwardinesh@yahoo.com>
2024-01-21 20:53:46 +0530

    dd

--- src/main/java/com/test/simplerest/SimpleRestApplication.java
+++ src/main/java/com/test/simplerest/SimpleRestApplication.java
@@ -1,7 +1,15 @@
 package com.test.simplerest;
 
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class SimpleRestApplication {
@@ -10,4 +18,17 @@ public class SimpleRestApplication {
 		SpringApplication.run(SimpleRestApplication.class, args);
 	}
 
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(Collections.singletonList("*"));
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean<CorsFilter> filterRegistration = new FilterRegistrationBean<>(new CorsFilter(source));
+		filterRegistration.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return filterRegistration;
+	}
 }
